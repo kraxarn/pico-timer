a593b706aac19d49b839e9dd9a0cfec588595676
kraxarn <kraxie@protonmail.com>
2024-08-18 18:55:20 +0200

    Add DIP switches

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 
 #define LED_DELAY_MS 1
 
-#define BUTTON_PIN 14
+#define BUTTON_PIN 15
+
+#define DIP1_PIN 11
+#define DIP2_PIN 12
+#define DIP3_PIN 13
+#define DIP4_PIN 14
 
 void pico_led_init()
 {
@@ -11,11 +16,11 @@ void pico_led_init()
 	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 }
 
-void button_init()
+void button_init(const uint gpio)
 {
-	gpio_init(BUTTON_PIN);
-	gpio_set_dir(BUTTON_PIN, GPIO_IN);
-	gpio_pull_down(BUTTON_PIN);
+	gpio_init(gpio);
+	gpio_set_dir(gpio, GPIO_IN);
+	gpio_pull_down(gpio);
 }
 
 void pico_set_led(const bool led_on)
@@ -23,19 +28,30 @@ void pico_set_led(const bool led_on)
 	gpio_put(PICO_DEFAULT_LED_PIN, led_on);
 }
 
-int main(void)
+int main()
 {
 	stdio_usb_init();
 
 	pico_led_init();
-	button_init();
+
+	button_init(BUTTON_PIN);
+	button_init(DIP1_PIN);
+	button_init(DIP2_PIN);
+	button_init(DIP3_PIN);
+	button_init(DIP4_PIN);
 
 	while (true)
 	{
 		const bool button_value = gpio_get(BUTTON_PIN);
 		pico_set_led(button_value);
 
-		printf("button_value: %d\n", button_value);
+		const bool dip1_value = gpio_get(DIP1_PIN);
+		const bool dip2_value = gpio_get(DIP2_PIN);
+		const bool dip3_value = gpio_get(DIP3_PIN);
+		const bool dip4_value = gpio_get(DIP4_PIN);
+
+		printf("btn0=%d dip1=%d dip2=%d dip3=%d dip4=%d\n", button_value,
+			dip1_value, dip2_value, dip3_value, dip4_value);
 
 		sleep_ms(LED_DELAY_MS);
 	}
